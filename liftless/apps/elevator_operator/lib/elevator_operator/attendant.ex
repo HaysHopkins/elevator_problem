63d0081404a48e880456847099713c09eda6a534
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2018-04-15 16:16:06 -0400

    Inefficient nearest elevator algorithm in place

--- liftless/apps/elevator_operator/lib/elevator_operator/attendant.ex
+++ liftless/apps/elevator_operator/lib/elevator_operator/attendant.ex
@@ -1,7 +1,6 @@
 defmodule ElevatorOperator.Attendant do
   use GenServer
 
-
   # API #
 
   def start_link(args) do
@@ -28,7 +27,7 @@ defmodule ElevatorOperator.Attendant do
                   %Elevator{name: floor, destination_queues: Map.new(request_queues), max_floor: top_floor}
                 end)
 
-    {:ok, %{elevators: elevators, request_queues: request_queues}}
+    {:ok, %{elevators: elevators, request_queues: request_queues, top_floor: top_floor}}
   end
 
   def handle_call(:get_state, _from, state) do
@@ -37,33 +36,80 @@ defmodule ElevatorOperator.Attendant do
 
   def handle_call({:request_lift, current_floor, destination}, _from, state) do
     current_statuses(state.elevators) |> IO.puts()
-    %Occupant{request_floor: current_floor, destination: destination} |> enqueue_rider() |> IO.puts()
-    # move_elevators
-    # current_statuses |> IO.puts()
-    {:reply, nil, state}
+
+    pre_move_state = %Occupant{request_floor: current_floor, destination: destination}
+                            |> enqueue_rider(state.request_queues)
+                            |> update_request_state(state)
+
+    find_nearest_elevator(current_floor, destination, state.elevators) |> IO.puts()
+
+    post_move_state = move_elevators(state.elevators)
+
+    current_statuses |> IO.puts()
+
+    {:reply, nil, pre_move_state}
   end
 
 
   # PRIVATE HELPER METHODS #
 
+
     # Status #
+
     defp current_statuses(elevators) do
       Enum.reduce(elevators, "", fn(el, msg) ->
         msg <> current_status(el)
       end)
     end
+
     defp current_status(el) do
       "\nElevator #{el.name} is currently on floor #{el.current_floor} and #{current_action(el.destination)}"
     end
+
     defp current_action(nil), do: "ready"
     defp current_action(destination), do: "in transit to #{destination}"
 
-    # Elevator Assignment #
-    defp enqueue_rider(occupant) do
 
+    # Elevator Requesting #
+
+    defp enqueue_rider(occupant, request_queues) do
+      %{request_queues | occupant.request_floor => [occupant | request_queues[occupant.request_floor]]}
     end
 
+    defp update_request_state(request_queues, state) do
+      %{state | request_queues: request_queues}
+    end
+
+
+    # Elevator Assigning #
+
+    defp find_nearest_elevator(request, destination, elevators) do
+      request - destination
+      |> codirectional(elevators)
+      |> select_closest(request)
+      |> format_closest()
+    end
+
+    defp codirectional(difference, elevators) when difference > 0, do: Elevator.ascending(elevators)
+    defp codirectional(difference, elevators) when difference < 0, do: Elevator.descending(elevators)
+
+    defp select_closest(elevators, request)
+      Map.reduce(elevators, nil, fn(el, nearest) ->
+        if closer?(el, nearest, request), do: el, else: nearest
+      end).name
+    end
+
+    def closer?(elevator, current, request) do
+      (abs(elevator.current_floor - request) < abs(el.current_floor - request))
+    end
+
+    def format_closest(name) do
+      "You will be picked up by elevator #{name}"
+    end
+
+
     # Elevator Movement #
+
     defp move_elevators do
 
     end
--- liftless/apps/elevator_operator/lib/elevator_operator/elevator.ex
+++ liftless/apps/elevator_operator/lib/elevator_operator/elevator.ex
@@ -1,3 +1,13 @@
 defmodule Elevator do
-  defstruct name: nil, destination_queues: %{}, current_floor: 0, destination: nil, max_floor: nil
+  defstruct name: nil, occupants: 0, destination_queues: %{}, current_floor: 0, destination: nil, max_floor: nil
+
+  def available?(elevator)
+    elevator.destination == nil
+  end
+
+  def incoming?(elevator, request, destination) do
+    case do
+
+    end
+  end
 end
