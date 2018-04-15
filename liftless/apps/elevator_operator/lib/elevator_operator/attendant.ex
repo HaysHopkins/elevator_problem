fea743661ea369ff078647e7cfe30f2c6d5e9eb9
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2018-04-15 13:22:41 -0400

    Elevator status methods complete

--- liftless/apps/elevator_operator/lib/elevator_operator/attendant.ex
+++ liftless/apps/elevator_operator/lib/elevator_operator/attendant.ex
@@ -3,12 +3,16 @@ defmodule ElevatorOperator.Attendant do
 
   # API #
 
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
   def get_state do
     GenServer.call(__MODULE__, :get_state)
   end
 
-  def start_link(args) do
-    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  def request_lift(current_floor, destination) do
+    GenServer.call(__MODULE__, {:request_lift, current_floor, destination})
   end
 
   # CALLBACKS #
@@ -21,4 +25,26 @@ defmodule ElevatorOperator.Attendant do
   def handle_call(:get_state, _from, state) do
     {:reply, state, state}
   end
+
+  def handle_call({:request_lift, current_floor, destination}, _from, state) do
+    current_statuses(state.elevators) |> IO.puts()
+    # %Occupant{} |> next_available_lift() |> IO.puts()
+    # move_elevators
+    # current_statuses |> IO.puts()
+    {:reply, nil, state}
+  end
+
+  # HELPER METHODS #
+
+    # Status
+    defp current_statuses(elevators) do
+      Enum.reduce(elevators, "", fn(el, msg) ->
+        msg <> current_status(el)
+      end)
+    end
+    defp current_status(el) do
+      "\nElevator #{el.name} is currently on floor #{el.current_floor} and #{current_action(el.destination)}"
+    end
+    defp current_action(nil), do: "ready"
+    defp current_action(destination), do: "in transit to #{destination}"
 end
--- liftless/apps/elevator_operator/lib/elevator_operator/elevator.ex
+++ liftless/apps/elevator_operator/lib/elevator_operator/elevator.ex
@@ -1,3 +1,3 @@
 defmodule Elevator do
-  defstruct name: nil, occupants: [], current_floor: 0, destination_floor: nil
+  defstruct name: nil, occupants: [], current_floor: 0, destination: nil
 end
