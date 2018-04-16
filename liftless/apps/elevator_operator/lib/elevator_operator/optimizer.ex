90c0cb6d24df10ef438a69a6916acc1dd906b515
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2018-04-15 21:53:10 -0400

    Refactor optimizer to consider destination as well as start

--- liftless/apps/elevator_operator/lib/elevator_operator/optimizer.ex
+++ liftless/apps/elevator_operator/lib/elevator_operator/optimizer.ex
@@ -1,8 +1,8 @@
 defmodule ElevatorOperator.Optimizer do
 
-  def find_nearest_elevator(elevators, request_floor, _) do
+  def find_nearest_elevator(elevators, request, request_dest) do
     Enum.map(elevators, fn(el)->
-      calculate_steps(true, request_floor, el.current_floor, [el.destination | el.next], 0)
+      calculate_steps({true, true}, request, request_dest, el.current_floor, [el.destination | el.next], 0)
       |> Tuple.append(el.name)
     end)
     |> Enum.min_by(fn({_, steps, _})-> steps end)
@@ -11,17 +11,30 @@ defmodule ElevatorOperator.Optimizer do
     #THEN COMPARE THAT TO TRUE STEPS STEP COUNTS + DIFF TO DESTINATION
   end
 
+  # Requests in range of current lift movements, return total
+  defp calculate_steps({false, false}, _, _, _, total), do: {{false, false}, total}
+  # Request in range, need to add destination because no more are in list
+  defp calculate_steps({false, true}, req_dest, last, [], total), do: {{false, true}, total + abs(last-req_dest)}
+  # Start calculating from destination once start is determined to be within range
+  # ONCE I PICK UP MIGHT WANT TO ESTABLISH BASE DIFFERENCE BASED ON BUILDING HEIGHT
+  # ON WHEN TO CONTINUE IN SAME DIRECTION, EVEN IF IT MEANS ALTERING SCHEDULE MIDWAY
+  defp calculate_steps({false, true}, request_dest, last, [next | next_steps], total) do
+    case request_dest in last..next do
+      true -> calculate_steps({false, false}, request_dest, next, next_steps, total + abs(last-request_dest))
+      false -> calculate_steps({false, true}, request_dest, next, next_steps, total + abs(last-next))
+    end
+  end
+
   # No destination, return total
-  defp calculate_steps(_, request_floor, current, [nil | _], _), do: {true, abs(current-request_floor)}
-  # request_floor in range of current lift movements, return total
-  defp calculate_steps(false, _, _, _, total), do: {false, total}
-  # No more steps, return total + steps to request_floor
-  defp calculate_steps(_, request_floor, _, [], total), do: {true, abs(total-request_floor)}
-  # request_floor in later range, return total
-  defp calculate_steps(continue, request_floor, last, [next | next_steps], total) do
-    case request_floor in last..next do
-      true -> calculate_steps(false, request_floor, next, next_steps, total + abs(last-request_floor))
-      false -> calculate_steps(continue, request_floor, next, next_steps, total + abs(last-next))
+  defp calculate_steps(_, request, request_dest, current, [nil | _], _),
+        do: {{true, true}, abs(current-request) + abs(request-request_dest)}
+  # Need to add both request and destination because netiher are in list
+  defp calculate_steps(_, req, req_dest, last, [], total), do: {{true, true}, total + abs(req-last) + abs(req-req_dest)}
+  # request in later range, return total
+  defp calculate_steps({add_start, add_end}, request, request_dest, last, [next | next_steps], total) do
+    case request in last..next do
+      true -> calculate_steps({false, true}, request_dest, next, next_steps, total + abs(last-request))
+      false -> calculate_steps({add_start, add_end}, request, request_dest, next, next_steps, total + abs(last-next))
     end
   end
 end
